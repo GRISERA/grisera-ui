d9c234d43cbd4f8e933814d22921cfc187b16159
Maczomen <maczomen2000@gmail.com>
2024-10-13 18:50:57 +0200

    fix: env variables in grisera ui

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 export default {
-  apiUrl: process.env.VUE_APP_API_URL || 'https://grisera-backend.affectivese.org',
-  authUrl: process.env.VUE_APP_AUTH_MS_URL || 'https://grisera-authms.affectivese.org/api',
+  // 'process.env.VUE_APP_API_URL' and 'process.env.VUE_APP_AUTH_MS_URL' are needed for entrypoint.sh script
+  // used to create prod image of grisera-ui using prod.dockerfile
+  apiUrl: process.env.VUE_APP_API_URL || 'process.env.VUE_APP_API_URL',
+  authUrl: process.env.VUE_APP_AUTH_MS_URL || 'process.env.VUE_APP_AUTH_MS_URL',
   sessionDurationMinutes: 30,
 };
