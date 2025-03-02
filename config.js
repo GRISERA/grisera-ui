b15a7dac169216d3e5537f59e843b29ea067d139
kamil <kamil.czepiell@gmail.com>
2025-03-02 13:56:49 +0100

    configuration adjustment

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 export default {
-  // 'process.env.VUE_APP_API_URL' and 'process.env.VUE_APP_AUTH_MS_URL' are needed for entrypoint.sh script
+  // 'VUE_APP_API_URL' and 'VUE_APP_AUTH_MS_URL' are needed for entrypoint.sh script
   // used to create prod image of grisera-ui using prod.dockerfile
-  apiUrl: process.env.VUE_APP_API_URL || 'process.env.VUE_APP_API_URL',
-  authUrl: process.env.VUE_APP_AUTH_MS_URL || 'process.env.VUE_APP_AUTH_MS_URL',
+  apiUrl: 'VUE_APP_API_URL' || 'http://localhost:8085',
+  authUrl: 'VUE_APP_AUTH_MS_URL' || 'http://localhost:8081/api',
+  keycloakUrl: 'VUE_APP_KEYCLOAK_URL' || 'http://localhost:8081/api',
+  keycloakClientId: 'VUE_APP_KEYCLOAK_CLIENT_ID' || 'grisera-ui',
+  realm: 'VUE_APP_KEYCLOAK_REALM' || 'grisera',
   sessionDurationMinutes: 30,
 };
