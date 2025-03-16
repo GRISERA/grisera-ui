ba3b5658897db8edd8c935074382b7dd3827bbf0
Arkadiusz Tepper <tepirek@outlook.com>
2025-03-16 18:08:06 +0100

    extend participant creation tests;

--- e2e-tests/tests/participant.spec.ts
+++ e2e-tests/tests/participant.spec.ts
@@ -8,7 +8,7 @@ test.beforeEach(async ({ page }) => {
     await new DatasetListPage(page).usingAnyDataset();
 });
 
-test('Użytkownik może utworzyć nowego uczestnika', async ({ page }) => {
+test('Użytkownik może utworzyć nowego uczestnika 1', async ({ page }) => {
     const participantPage = new ParticipantCreatePage(page);
     await participantPage.visit();
     await participantPage.fillParticipantForm(
@@ -21,4 +21,34 @@ test('Użytkownik może utworzyć nowego uczestnika', async ({ page }) => {
 
     await expect(page).toHaveURL('/participants');
     await expect(page.locator('text=James Anderson').last()).toBeVisible();
+});
+
+test('Użytkownik może utworzyć nowego uczestnika 2', async ({ page }) => {
+    const participantPage = new ParticipantCreatePage(page);
+    await participantPage.visit();
+    await participantPage.fillParticipantForm(
+        'Emma',
+        'Carter',
+        '1987-03-01',
+        'Female',
+    );
+    await participantPage.submitForm();
+
+    await expect(page).toHaveURL('/participants');
+    await expect(page.locator('text=Emma Carter').last()).toBeVisible();
+});
+
+test('Użytkownik może utworzyć nowego uczestnika 3', async ({ page }) => {
+    const participantPage = new ParticipantCreatePage(page);
+    await participantPage.visit();
+    await participantPage.fillParticipantForm(
+        'Caroline',
+        'Kane',
+        '1999-01-01',
+        'Female',
+    );
+    await participantPage.submitForm();
+
+    await expect(page).toHaveURL('/participants');
+    await expect(page.locator('text=Caroline Kane').last()).toBeVisible();
 });
