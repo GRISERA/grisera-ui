32204979bdbe3a7a3866983b27229fafb3592932
Arkadiusz Tepper <tepirek@outlook.com>
2025-08-22 22:14:00 +0200

    add missing roles config

--- src/acl/roles/index.js
+++ src/acl/roles/index.js
@@ -1,6 +1,10 @@
 import aclRoleNames from '@/acl/acl-role-names';
+import editor from '@/acl/roles/editor';
 import owner from '@/acl/roles/owner';
+import reader from '@/acl/roles/reader';
 
 export default {
+  [aclRoleNames.READER]: reader,
+  [aclRoleNames.EDITOR]: editor,
   [aclRoleNames.OWNER]: owner,
 };
