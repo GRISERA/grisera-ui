dceb3256c6836a3af34e440b715d665ec5a3304a
Arkadiusz Tepper <tepirek@outlook.com>
2025-08-24 18:39:36 +0200

    refactor ImportsView to improve UI/UX with empty state component and enhanced button interactions

--- src/views/imports/ImportsView.vue
+++ src/views/imports/ImportsView.vue
@@ -7,124 +7,129 @@
       </v-col>
       <v-col class="text-right">
         <v-btn
-          :disabled="!currentDatasetId"
-          :outlined="true"
-          @click="$router.push({ name: 'import-creation' })"
+          class="text-none font-weight-medium"
+          color="primary"
+          rounded
+          @click.prevent.stop="$router.push({ name: 'import-creation' })"
         >
-          {{ currentDatasetId ? 'Create' : 'Select Dataset First' }}
+          <v-icon left>
+            mdi-plus
+          </v-icon>
+          Create New Import
         </v-btn>
       </v-col>
       <v-col class="col-12">
-        <v-container class="container--fluid">
-          <v-row v-if="!currentDatasetId">
-            <v-col class="col-12 text-center">
-              <p class="title grey--text">
-                Please select a dataset first to see its imports.
-              </p>
-            </v-col>
-          </v-row>
-          <v-row
-            v-else-if="isLoading"
-            align-content="center"
-            class="fill-height"
-            justify="center"
+        <v-row v-if="!currentDatasetId">
+          <v-col class="col-12 text-center">
+            <p class="title grey--text">
+              Please select a dataset first to see its imports.
+            </p>
+          </v-col>
+        </v-row>
+        <v-row
+          v-else-if="isLoading"
+          align-content="center"
+          class="fill-height"
+          justify="center"
+        >
+          <v-col
+            class="subtitle-1 text-center"
+            cols="12"
           >
-            <v-col
-              class="subtitle-1 text-center"
-              cols="12"
-            >
-              Loading imports...
-            </v-col>
-            <v-col cols="6">
-              <v-progress-linear
-                height="6"
-                indeterminate
-                rounded
-              />
-            </v-col>
-          </v-row>
-          <v-row v-else-if="imports.length === 0">
-            <v-col class="col-12 text-center">
-              <p class="title grey--text">
-                No imports found for this dataset.
-              </p>
-            </v-col>
-          </v-row>
-          <v-row
-            v-else
-            class="fill-height"
+            Loading imports...
+          </v-col>
+          <v-col cols="6">
+            <v-progress-linear
+              height="6"
+              indeterminate
+              rounded
+            />
+          </v-col>
+        </v-row>
+        <!-- Empty State -->
+        <empty-state
+          v-else-if="imports.length === 0"
+          action-icon="mdi-plus"
+          action-text="Create Your First Import"
+          description="No imports found for this dataset. Start by creating your first data import to begin working with your data."
+          icon="mdi-database-import"
+          title="No Imports Found"
+          @action="navigateToCreateImport"
+        />
+        <v-row
+          v-else
+          class="fill-height"
+        >
+          <v-col
+            v-for="importJob in imports"
+            :key="`import_${importJob.id}`"
+            class="col-md-4 col-sm-6 col-12 align-self-stretch"
           >
-            <v-col
-              v-for="importJob in imports"
-              :key="`import_${importJob.id}`"
-              class="col-md-4 col-sm-6 col-12 align-self-stretch"
+            <v-card
+              :elevation="4"
+              class="d-flex flex-column"
+              height="100%"
             >
-              <v-card
-                :elevation="4"
-                class="d-flex flex-column"
-                height="100%"
-              >
-                <v-card-title class="pb-0 mb-5">
-                  <v-icon left>
-                    {{ getFileIcon(importJob.file_name) }}
+              <v-card-title class="pb-0 mb-5">
+                <v-icon left>
+                  {{ getFileIcon(importJob.file_name) }}
+                </v-icon>
+                <span class="subtitle-1 font-weight-medium">{{ importJob.file_name }}</span>
+              </v-card-title>
+              <v-card-subtitle class="pt-0 text-caption">
+                <span class="d-block">ID: <span class="import-id-ellipsis">{{ importJob.id }}</span></span>
+                <span class="d-block">Created: {{ formatDate(importJob.created_at) }}</span>
+              </v-card-subtitle>
+              <v-card-text class="flex-grow-1">
+                <div class="caption">
+                  Description:
+                </div>
+                <div class="black--text text-body-2">
+                  {{ importJob.description || '-' }}
+                </div>
+                <div class="caption mt-2">
+                  Status:
+                </div>
+                <v-chip
+                  :color="getStatusColor(importJob.status)"
+                  class="font-weight-bold"
+                  label
+                  small
+                  text-color="white"
+                >
+                  {{ importJob.status }}
+                </v-chip>
+              </v-card-text>
+              <v-divider />
+              <v-card-actions>
+                <v-btn
+                  :loading="isRefreshing[importJob.id]"
+                  icon
+                  small
+                  @click="refreshSingleImportStatus(importJob)"
+                >
+                  <v-icon>mdi-refresh</v-icon>
+                </v-btn>
+                <v-spacer />
+                <v-btn
+                  :disabled="true"
+                  color="red"
+                  small
+                  text
+                  @click="deleteImport(importJob.id)"
+                >
+                  <v-icon
+                    left
+                    small
+                  >
+                    mdi-delete
                   </v-icon>
-                  <span class="subtitle-1 font-weight-medium">{{ importJob.file_name }}</span>
-                </v-card-title>
-                <v-card-subtitle class="pt-0 text-caption">
-                  <span class="d-block">ID: <span class="import-id-ellipsis">{{ importJob.id }}</span></span>
-                  <span class="d-block">Created: {{ formatDate(importJob.created_at) }}</span>
-                </v-card-subtitle>
-                <v-card-text class="flex-grow-1">
-                  <div class="caption">
-                    Description:
-                  </div>
-                  <div class="black--text text-body-2">
-                    {{ importJob.description || '-' }}
-                  </div>
-                  <div class="caption mt-2">
-                    Status:
-                  </div>
-                  <v-chip
-                    :color="getStatusColor(importJob.status)"
-                    class="font-weight-bold"
-                    label
-                    small
-                    text-color="white"
-                  >
-                    {{ importJob.status }}
-                  </v-chip>
-                </v-card-text>
-                <v-divider />
-                <v-card-actions>
-                  <v-btn
-                    :loading="isRefreshing[importJob.id]"
-                    icon
-                    small
-                    @click="refreshSingleImportStatus(importJob)"
-                  >
-                    <v-icon>mdi-refresh</v-icon>
-                  </v-btn>
-                  <v-spacer />
-                  <v-btn
-                    :disabled="true"
-                    color="red"
-                    small
-                    text
-                    @click="deleteImport(importJob.id)"
-                  >
-                    <v-icon
-                      left
-                      small
-                    >
-                      mdi-delete
-                    </v-icon>
-                    Cancel
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
+                  Cancel
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
 
@@ -142,6 +147,7 @@
 <script>
 import ImportAPI from '@/api/ImportAPI';
 import AppBreadcrumbs from '@/components/AppBreadcrumbs.vue';
+import EmptyState from '@/components/EmptyState.vue';
 import InfoToolTipComponent from '@/components/InfoToolTipComponent.vue';
 import { mapActions, mapGetters, mapState } from 'vuex';
 
@@ -150,6 +156,7 @@ export default {
   components: {
     InfoToolTipComponent,
     AppBreadcrumbs,
+    EmptyState,
   },
   data: () => (
     {
@@ -268,6 +275,13 @@ export default {
     },
     isPermanentStatus(status) {
       return ['completed', 'failed'].includes(status);
+    },
+    navigateToCreateImport() {
+      if (this.currentDatasetId) {
+        this.$router.push({ name: 'import-creation' });
+      } else {
+        this.showSnackbar('Please select a dataset first', 'warning');
+      }
     },
   },
 };
